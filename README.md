7ac112874de319cf55fe6fc43ecb2c224f38730b
alexleybourne <alexleybourne1@gmail.com>
2023-04-01 21:37:36 +1000

    Initial setup and code cleanup 🧹

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# google-chatgpt
-A Google Chrome extension that swaps the I'm Feeling Lucky button for ChatGPT 🤖
+# Google ChatGPT
+
+A Google Chrome extension that swaps the `I'm Feeling Lucky` button for ChatGPT 🤖
+
+## Features
+
+- Removes the _"I'm Feeling Lucky"_ button
+- You can also use the new shiny `ChatGPT` button
+
+## How to install
+
+1. go to: `chrome://extensions/` in your browser
+2. Turn on developer mode in the top right-hand side
+3. Click on `Load unpacked` in the top left-hand side
+4. Select the folder with the extensions code
+5. Now you can use chatgpt from the google homepage 😎
---
+++ script.js
@@ -0,0 +1,134 @@
+// Checking if the page is google search but not a google search results page
+const URL = window.location.href;
+const includesGoogle = URL.includes('www.google.com');
+const canRun = !URL.includes('search') && includesGoogle;
+
+const teal = '#74A99C;';
+
+const getSearchValue = () => {
+  const searchValue = document.querySelector('[title="Search"]').value;
+  // If the value is not blank it continues
+  if (searchValue.length) {
+    // Replaces spaces in the query with pluses to work in url query
+    const formattedValue = searchValue.replace(/\s/g, '+');
+    // Sends the cleaned up value back
+    return formattedValue;
+  }
+};
+
+// Edits the suggestions google gives so they can send you to stack overflow too
+const setToTeal = (selector, stackSearch) => {
+  document.querySelectorAll(selector).forEach(function (item) {
+    if (stackSearch) {
+      item.classList.add('Teal-Text');
+    } else {
+      item.classList.remove('Teal-Text');
+    }
+  });
+};
+
+const stackSearch = () => {
+  // gets the input value from the function
+  const search = getSearchValue();
+  // checks it has returned a value
+  if (search) {
+    // visits the search
+    window.location.href = `https://stackoverflow.com/search?q=${search}`;
+  }
+};
+
+window.onbeforeunload = function (e) {
+  if (!includesGoogle) return;
+  return e.preventDefault();
+};
+
+document.addEventListener(
+  'click',
+  (e) => {
+    // Don't want it running on google search / result pages
+    if (!includesGoogle) return;
+    // If we have not clicked on the stack overflow button we want the page
+    // to behave the same as normal
+    if (!e.target.matches('.ChatGPT-Button')) return;
+    // Prevent default click action that google does ( searches )
+    e.preventDefault();
+    // Now we run our search Function
+    stackSearch();
+  },
+  false
+);
+
+const searchPrefix = `/c`;
+
+document.addEventListener('keyup', (e) => {
+  // The search and results page both use the same input title
+  // This code can run on both (⌐■_■)
+  if (!includesGoogle) return;
+  // Gets the search value, checks it has a value and checks for the "/s" command
+  const search = getSearchValue();
+  const stackSearch = search?.includes(searchPrefix);
+  // Search Field text
+  setToTeal('[title="Search"]', stackSearch);
+  // Suggestion text
+  setToTeal('[role="option"]', stackSearch);
+  // Checking it can run and it is the enter key
+  if (e.code == 'Enter' && stackSearch) {
+    e.preventDefault();
+    // removes "/s+" from the start of our query
+    const searchCleaned = search.trim().replace(searchPrefix, '');
+    window.location.href = `https://stackoverflow.com/search?q=${searchCleaned}`;
+  }
+});
+
+const rgbToHex = (rgb) => {
+  const [r, g, b] = rgb.match(/\d+/g).map(Number);
+  return '#' + [r, g, b].map((x) => x.toString(16).padStart(2, '0')).join('');
+};
+
+// This runs if this is the correct page
+if (canRun) {
+  // Grabs the buttons color to account for dark mode and any styling changes
+  const luckyButton = document.querySelector(
+    'input[value="I\'m Feeling Lucky"]'
+  );
+  console.log('lucky button: ', luckyButton);
+  // const input = document.querySelector('input[value="I\'m Feeling Lucky"]');
+  // const colour = luckyButton.style.color;
+  // console.log('color: ', colour);
+  // const backgroundColour = luckyButton.style.backgroundColor;
+  // console.log('background colour: ', backgroundColour);
+  const colour = window.getComputedStyle(luckyButton).getPropertyValue('color');
+  const backgroundColour = window
+    .getComputedStyle(luckyButton)
+    .getPropertyValue('background-color');
+
+  console.log('color: ', colour);
+  console.log('background colour: ', backgroundColour);
+
+  // Finding each "I'm feeling lucky" Button
+  var inputs = document.querySelectorAll('input[name="btnI"]');
+  for (i = 0; i < inputs.length; i++) {
+    // This is the main home screen Button that is going to be animated
+    if (i == 1) {
+      // Creating our animated button so there is no jump on load
+      const ButtonAnimated = document.createElement('div');
+      ButtonAnimated.classList.add('Button-Container-Viewport');
+      ButtonAnimated.innerHTML = `
+                <div class="Button-Container">
+                    <button class="Button" style="color: ${colour}; background-color: ${backgroundColour};">I'm Feeling Lucky</button>
+                    <button onclick="stackSearch()" class="ChatGPT-Button Button">ChatGPT</button>
+                </div>
+            `;
+      // replacing the feeling lucky button
+      inputs[i].replaceWith(ButtonAnimated);
+    } else {
+      // Creating the new stack overflow button
+      const newButton = document.createElement('button');
+      newButton.innerHTML = 'ChatGPT';
+      newButton.classList.add('ChatGPT-Button', 'Button');
+      newButton.onclick = () => stackSearch();
+      // replacing the feeling lucky button
+      inputs[i].replaceWith(newButton);
+    }
+  }
+}
